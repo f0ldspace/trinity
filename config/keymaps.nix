3258d0740fed8181f6a3967b3e526a1dbf0f073f
f0ld <me@f0ld.space>
2025-12-30 09:29:38 +0000

    better buffer bindings

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -181,6 +181,27 @@
                   desc = "Previous buffer";
                 };
               };
+              "<leader>bf" = {
+                action = "<cmd>Telescope buffers<CR>";
+                options = {
+                  desc = "Find buffer";
+                };
+              };
+              "<leader>bx" = {
+                action.__raw = ''
+                  function()
+                    local current = vim.api.nvim_get_current_buf()
+                    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                      if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+                        vim.api.nvim_buf_delete(buf, { force = true })
+                      end
+                    end
+                  end
+                '';
+                options = {
+                  desc = "Close all buffers except current";
+                };
+              };
               "<leader>wd" = {
                 action = "<C-W>c";
                 options = {
