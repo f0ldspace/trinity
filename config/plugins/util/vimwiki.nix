d5b4357014e81fef7c282ac06635dac45c2e0bc5
f0ld <me@f0ld.space>
2026-01-02 20:47:13 +0000

    vimwiki paths

--- config/plugins/util/vimwiki.nix
+++ config/plugins/util/vimwiki.nix
@@ -5,15 +5,14 @@
     settings = {
       list = [
         {
-          name = "personal";
-          path = "~/personal/";
-          syntax = "markdown";
-          ext = ".md";
-          diary_rel_path = "";
-        }
-        {
           name = "work";
           path = "~/vimwiki/";
+          syntax = "markdown";
+          ext = ".md";
+        }
+        {
+          name = "personal";
+          path = "~/personal/";
           syntax = "markdown";
           ext = ".md";
         }
